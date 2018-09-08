5a138bcc14e5a4e5cecf94fe955e7c1a062d6092
sangumee <super2451894@gmail.com>
2018-09-08 20:27:47 +0900

    Build Admin Panel
    Fix index.html to design AdminPanel
    Fix App.js to Load and Hide AdminPanel
    Fix some CSS code to fix Button

--- js/app.js
+++ js/app.js
@@ -1,38 +1,36 @@
-
 /* ======= Model ======= */
 
 var model = {
     currentCat: null,
-    cats: [
-        {
-            clickCount : 0,
-            name : 'Tabby',
-            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
+    cats: [{
+            clickCount: 0,
+            name: 'Tabby',
+            imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568'
         },
         {
-            clickCount : 0,
-            name : 'Tiger',
-            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
+            clickCount: 0,
+            name: 'Tiger',
+            imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904'
         },
         {
-            clickCount : 0,
-            name : 'Scaredy',
-            imgSrc : 'img/22252709_010df3379e_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
+            clickCount: 0,
+            name: 'Scaredy',
+            imgSrc: 'img/22252709_010df3379e_z.jpg',
+            imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709'
         },
         {
-            clickCount : 0,
-            name : 'Shadow',
-            imgSrc : 'img/1413379559_412a540d29_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
+            clickCount: 0,
+            name: 'Shadow',
+            imgSrc: 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559'
         },
         {
-            clickCount : 0,
-            name : 'Sleepy',
-            imgSrc : 'img/9648464288_2516b35537_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
+            clickCount: 0,
+            name: 'Sleepy',
+            imgSrc: 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288'
         }
     ]
 };
@@ -42,7 +40,7 @@ var model = {
 
 var octopus = {
 
-    init: function() {
+    init: function () {
         // set our current cat to the first one in the list
         model.currentCat = model.cats[0];
 
@@ -51,23 +49,32 @@ var octopus = {
         catView.init();
     },
 
-    getCurrentCat: function() {
+    getCurrentCat: function () {
         return model.currentCat;
     },
 
-    getCats: function() {
+    getCats: function () {
         return model.cats;
     },
 
     // set the currently-selected cat to the object passed in
-    setCurrentCat: function(cat) {
+    setCurrentCat: function (cat) {
         model.currentCat = cat;
     },
 
     // increments the counter for the currently-selected cat
-    incrementCounter: function() {
+    incrementCounter: function () {
         model.currentCat.clickCount++;
         catView.render();
+    },
+    // Load or Hide Admin Panel
+    adminPanel: function () {
+        var con = document.getElementById('adminPanel');
+        if (con.style.display == 'none') {
+            con.style.display = 'block';
+        } else {
+            con.style.display = 'none';
+        }
     }
 };
 
@@ -76,15 +83,16 @@ var octopus = {
 
 var catView = {
 
-    init: function() {
+    init: function () {
         // store pointers to our DOM elements for easy access later
         this.catElem = document.getElementById('cat');
         this.catNameElem = document.getElementById('cat-name');
         this.catImageElem = document.getElementById('cat-img');
         this.countElem = document.getElementById('cat-count');
+        this.adminButton = document.getElementById('admin');
 
         // on click, increment the current cat's counter
-        this.catImageElem.addEventListener('click', function(){
+        this.catImageElem.addEventListener('click', function () {
             octopus.incrementCounter();
         });
 
@@ -92,7 +100,7 @@ var catView = {
         this.render();
     },
 
-    render: function() {
+    render: function () {
         // update the DOM elements with values from the current cat
         var currentCat = octopus.getCurrentCat();
         this.countElem.textContent = currentCat.clickCount;
@@ -103,7 +111,7 @@ var catView = {
 
 var catListView = {
 
-    init: function() {
+    init: function () {
         // store the DOM element for easy access later
         this.catListElem = document.getElementById('cat-list');
 
@@ -111,7 +119,7 @@ var catListView = {
         this.render();
     },
 
-    render: function() {
+    render: function () {
         var cat, elem, i;
         // get the cats we'll be rendering from the octopus
         var cats = octopus.getCats();
@@ -124,15 +132,15 @@ var catListView = {
             // this is the cat we're currently looping over
             cat = cats[i];
 
-            // make a new cat list item and set its text
-            elem = document.createElement('li');
+            // make a new cat button item and set its text
+            elem = document.createElement('button');
             elem.textContent = cat.name;
 
             // on click, setCurrentCat and render the catView
             // (this uses our closure-in-a-loop trick to connect the value
             //  of the cat variable to the click event function)
-            elem.addEventListener('click', (function(catCopy) {
-                return function() {
+            elem.addEventListener('click', (function (catCopy) {
+                return function () {
                     octopus.setCurrentCat(catCopy);
                     catView.render();
                 };
@@ -140,6 +148,7 @@ var catListView = {
 
             // finally, add the element to the list
             this.catListElem.appendChild(elem);
+
         }
     }
 };
